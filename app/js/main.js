1495e3d4823d3849d7af3d25186269216439621a
Petrov012110 <grisha14.06.96@gmail.com>
2020-10-25 20:41:42 +0500

    block travel was done

--- app/js/main.js
+++ app/js/main.js
@@ -6,14 +6,37 @@ $('.header__slider').slick({
     fade: true,
     prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt="">',
     nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt="">',
-    asNavFor: '.slider-dots'
+    asNavFor: '.slider-dotshead'
 
 });
 
-$('.slider-dots').slick({
+$('.slider-dotshead').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    asNavFor: '.header__slider'
+});
+  
+$('.surf-slider').slick({
     slidesToShow: 4,
     slidesToScroll: 1,
-    asNavFor: '.header__slider'
-})
-  
+    prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt="">',
+    nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt="">',
+    asNavFor: '.slider-map',
+});
+
+$('.slider-map').slick({
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    arrows: false,
+    asNavFor: '.surf-slider',
+    focusOnSelect: true
+});
+
+$('.travel__slider').slick({
+    fade: true,
+    infinite: true,
+    prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt="">',
+    nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt="">',
+});
+
 });
